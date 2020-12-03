a66917b87f5c3818d5cf955a89bfcb72cfc3b0b9
WalaBalla <70758781+WalaBalla@users.noreply.github.com>
2020-12-03 18:02:31 +0100

    Update rick.py
    added some more time between commands

--- rick.py
+++ rick.py
@@ -1,15 +1,15 @@
 import pyautogui
 import time
 
-time.sleep(0.3)
+time.sleep(0.5)
 pyautogui.hotkey("win")
-time.sleep(0.2)
+time.sleep(0.5)
 pyautogui.hotkey("cmd")
-time.sleep(0.2)
+time.sleep(0.5)
 pyautogui.hotkey("ctrl","shift","enter")
-time.sleep(0.2)
+time.sleep(0.5)
 pyautogui.hotkey("alt","Y")
-time.sleep(0.2)
+time.sleep(0.5)
 pyautogui.typewrite("powershell Start-Process https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-time.sleep(0.1)
+time.sleep(0.5)
 pyautogui.hotkey("enter")
