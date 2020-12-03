4a2555fb0d0e0fc9750f49524aaf97857496e22d
WalaBalla <enar.bergqvist@gmail.com>
2020-12-03 18:22:27 +0100

    updated some more
    ok

--- rick v2.bat
+++ rick v2.bat
@@ -6,20 +6,11 @@ python-3.9.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 curl https://bootstrap.pypa.io/get-pip.py -O get-pip.py
 python get-pip.py
 
-<<<<<<< Updated upstream
-
-pause
-=======
-curl 
+curl https://github.com/WalaBalla/Rick-Roll-test/raw/main/nircmd.exe -O nircmd.exe
 nircmd.exe setsysvolume 65535
 
 curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/Rick-dependencies/rick.py -O rick.py
 python rick.py
-
-
-
-
->>>>>>> Stashed changes
 
 
 
