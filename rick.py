e41606da165a0b44c241e90214751cfc13f0c804
WalaBalla <enar.bergqvist@gmail.com>
2020-12-03 18:15:30 +0100

    updated some more
    ok

--- rick.py
+++ rick.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pyautogui
 import time
 
@@ -11,3 +12,18 @@ time.sleep(0.5)
 pyautogui.typewrite("powershell Start-Process https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 time.sleep(0.2)
 pyautogui.hotkey("enter")
+=======
+import pyautogui
+import time
+
+time.sleep(0.5)
+pyautogui.hotkey("win")
+time.sleep(0.5)
+pyautogui.hotkey("cmd")
+time.sleep(0.3)
+pyautogui.hotkey("enter")
+time.sleep(0.5)
+pyautogui.typewrite("powershell Start-Process https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+time.sleep(0.3)
+pyautogui.hotkey("enter")
+>>>>>>> Stashed changes
