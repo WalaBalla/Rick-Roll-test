125f317f7e9151e4daf467b9d495cb0f655fa8df
WalaBalla <enar.bergqvist@gmail.com>
2020-12-04 23:13:56 +0100

    ok fett bruh upgrade

---
+++ rick v3.bat
@@ -0,0 +1,8 @@
+curl https://www.nirsoft.net/utils/nircmd-x64.zip -O
+powershell Expand-Archive -Force nircmd-x64.zip C:\Users\%username%\Downloads\
+
+C:\Users\%username%\Downloads\nircmd.exe setsysvolume 65535
+
+start https://youtu.be/DLzxrzFCyOs
+
+pause
