74a36e5a49699c02d6b078b470a39a9b98dd1075
WalaBalla <enar.bergqvist@gmail.com>
2020-12-05 13:52:26 +0100

    testing

--- rick v4 hardcore.bat
+++ rick v4 hardcore.bat
@@ -4,9 +4,11 @@ powershell Expand-Archive -Force nircmd-x64.zip C:\Users\%username%\Downloads\
 C:\Users\%username%\Downloads\nircmd.exe setsysvolume 65535
 
 taskkill /F /IM "wallpaper32.exe" /T
-curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/main/maxresdefault.bmp -O maxresdefault.bmp
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d maxresdefault.bmp /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/main/maxresdefault.jpg -O maxresdefault.jpg
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\%username%\Downloads\maxresdefault.jpg /f
+C:\Windows\System32\rundll32.exe C:\Windows\System32\user32.dll, UpdatePerUserSystemParameters
+
+pause
 
 curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/main/RickStart.bat -O RickStart.bat
 start RickStart.bat
