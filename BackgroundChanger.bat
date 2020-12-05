aaeda3098e1f3ba830828c99b50845cc35d950a7
WalaBalla <enar.bergqvist@gmail.com>
2020-12-05 15:08:13 +0100

    functioning
    changes wallpaper to rick and starts it at 100% volume

--- BackgroundChanger.bat
+++ BackgroundChanger.bat
@@ -1,3 +1,8 @@
+cd C:\Users\%username%\Downloads\
 curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/main/maxresdefault.jpg -O maxresdefault.jpg
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\%username%\Downloads\maxresdefault.jpg /f
 C:\Windows\System32\rundll32.exe C:\Windows\System32\user32.dll, UpdatePerUserSystemParameters
+
+cd C:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine\
+wallpaper32.exe -control openWallpaper -file C:\Users\%username%\Downloads\maxresdefault.jpg
+cd C:\Users\%username%\Downloads\
--- rick v4 hardcore.bat
+++ rick v4 hardcore.bat
@@ -1,7 +1,3 @@
-cd C:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine\
-wallpaper32.exe -control stop
-cd C:\Users\%username%\Downloads\
-
 curl https://www.nirsoft.net/utils/nircmd-x64.zip -O
 powershell Expand-Archive -Force nircmd-x64.zip C:\Users\%username%\Downloads\
 
@@ -14,4 +10,11 @@ start RickStart.bat
 curl https://raw.githubusercontent.com/WalaBalla/Rick-Roll-test/main/BackgroundChanger.bat -O 
 start BackgroundChanger.bat
 
-
+cd C:\Users\%username%\Downloads\
+del C:\Users\%username%\Downloads\nircmd.exe
+del C:\Users\%username%\Downloads\nircmdc.exe
+del C:\Users\%username%\Downloads\NirCmd.chm
+del C:\Users\%username%\Downloads\RickStart.bat
+del C:\Users\%username%\Downloads\BackgroundChanger.bat
+del C:\Users\%username%\Downloads\nircmd-x64.zip
+del C:\Users\%username%\Downloads\maxresdefault.jpg
